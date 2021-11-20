ee765042daa73a2eab8de76e2e9e05aff79b764d
Ramadhan Febri <rafeta02@student.uns.ac.id>
2021-11-20 16:20:09 +0700

    slice page : index, login, checkout, success-checkout

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name('index');
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('/checkout', function () {
+    return view('checkout');
+})->name('checkout');
+
+Route::get('/success-checkout', function () {
+    return view('success_checkout');
+})->name('success-checkout');

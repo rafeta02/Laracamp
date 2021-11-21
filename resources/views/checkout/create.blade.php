1b8f4316c814b7653b997f42b147730ec3b1928b
Ramadhan Febri <rafeta02@student.uns.ac.id>
2021-11-21 22:11:19 +0700

    integration done

--- resources/views/checkout/create.blade.php
+++ resources/views/checkout/create.blade.php
@@ -20,8 +20,8 @@
                     <div class="col-lg-5 col-12">
                         <div class="item-bootcamp">
                             <img src="{{ asset('images/item_bootcamp.png') }}" alt="" class="cover">
-                            <h1 class="package">
-                                GILA BELAJAR
+                            <h1 class="package text-uppercase">
+                                {{ $camp->title}}
                             </h1>
                             <p class="description">
                                 Bootcamp ini akan mengajak Anda untuk belajar penuh mulai dari pengenalan dasar sampai membangun sebuah projek asli
@@ -30,32 +30,33 @@
                     </div>
                     <div class="col-lg-1 col-12"></div>
                     <div class="col-lg-6 col-12">
-                        <form action="{{ route('success-checkout') }}" class="basic-form">
+                        <form action="{{ route('checkout.store', $camp->id) }}" class="basic-form" method="POST">
+                            @csrf
                             <div class="mb-4">
-                                <label for="exampleInputEmail1" class="form-label">Full Name</label>
-                                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                <label for="name" class="form-label">Full Name</label>
+                                <input name="name" value="{{ Auth::user()->name }}" type="text" class="form-control" id="name" aria-describedby="fullName">
                             </div>
                             <div class="mb-4">
-                                <label for="exampleInputEmail1" class="form-label">Email Address</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                <label for="email" class="form-label">Email Address</label>
+                                <input name="email" value="{{ Auth::user()->email }}" type="email" class="form-control" id="email" aria-describedby="email">
                             </div>
                             <div class="mb-4">
-                                <label for="exampleInputEmail1" class="form-label">Occupation</label>
-                                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                <label for="occupation" class="form-label">Occupation</label>
+                                <input name="occupation" value="{{ Auth::user()->occupation }}" type="text" class="form-control" id="occupation" aria-describedby="occupation">
                             </div>
                             <div class="mb-4">
-                                <label for="exampleInputEmail1" class="form-label">Card Number</label>
-                                <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                <label for="card_number" class="form-label">Card Number</label>
+                                <input name="card_number" type="number" class="form-control" id="card_number" aria-describedby="cardNumber">
                             </div>
                             <div class="mb-5">
                                 <div class="row">
                                     <div class="col-lg-6 col-12">
-                                        <label for="exampleInputEmail1" class="form-label">Expired</label>
-                                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                        <label for="expired" class="form-label">Expired</label>
+                                        <input name="expired" type="month" class="form-control" id="expired" aria-describedby="expired">
                                     </div>
                                     <div class="col-lg-6 col-12">
-                                        <label for="exampleInputEmail1" class="form-label">CVC</label>
-                                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                        <label for="cvc" class="form-label">CVC</label>
+                                        <input name="cvc" type="text" maxlength="3" class="form-control" id="cvc" aria-describedby="cvc">
                                     </div>
                                 </div>
                             </div>
